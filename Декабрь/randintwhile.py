5d4f022d1db7878ed18b7c18ba0f63441cf08043
volpol503 <70723886+volpol503@users.noreply.github.com>
2023-03-10 13:23:40 +0300

    Create randintwhile.py

---
+++ Декабрь/randintwhile.py
@@ -0,0 +1,21 @@
+from random import randint
+N = 10
+a = []
+for i in range (N):
+  a.append(randint(1,99))
+print(a)
+
+
+
+
+
+i = 0
+while i < N - 1:
+  j = 0
+  while j < N - 1 - i:
+    if a[j] > a[j + 1]:
+      a[j], a[j + 1] = a[j + 1], a[j]
+    j += 1
+  i += 1
+  
+print(a)
